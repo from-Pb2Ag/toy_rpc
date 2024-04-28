4c7310d7b34c37d2d6e6a4eb89bd9f57fafb4af2
from-Pb2Ag <cn.qyuler@gmail.com>
2024-04-28 23:42:55 +0800

    update some in-line comments

--- example/callee/friendservice.cpp
+++ example/callee/friendservice.cpp
@@ -39,14 +39,11 @@ int main(int argc, char **argv) {
     LOG_ERR("ddddd");
     LOG_INFO("ddddd");
 
-    // 调用框架的初始化操作
     RpcApplication::Init(argc, argv);
 
-    // provider是一个rpc网络服务对象。把UserService对象发布到rpc节点上
     RpcProvider provider;
     provider.NotifyService(new FriendService());
 
-    // 启动一个rpc服务发布节点   Run以后，进程进入阻塞状态，等待远程的rpc调用请求
     provider.Run();
 
     return 0;
--- src/include/rpcapplication.h
+++ src/include/rpcapplication.h
@@ -7,11 +7,14 @@
 
 class RpcApplication {
     public:
+        // load configuration with path as hashmap.
         static void Init(int argc, char **argv);
         static RpcApplication& GetInstance();
         static RpcConfig& GetConfig();
     private:
         static RpcConfig m_config;
+        // for Singleton Pattern, we make construction function unavailable,
+        // and ban the copy/move assignment function/operator.
 
         RpcApplication(){}
         RpcApplication(const RpcApplication&) = delete;
